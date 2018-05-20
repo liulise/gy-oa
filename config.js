9465ae25420b265833a333c972b66cbfdb9d6142
liulise <980168151@qq.com>
2018-05-20 11:48:02 +0800

    delete extract-text-webpack-plugin

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const isDev = NODE_ENV === 'development';
 const webpack = require('webpack');
 const manifest = require('./vendor-manifest.json');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   isDev,
@@ -23,7 +23,7 @@ module.exports = {
         loader: 'eslint-loader'
       },
       {
-        test: /\.(s?css)/,
+        test: /\.(s?css)$/,
         use: [
           'style-loader',
           {
@@ -48,37 +48,36 @@ module.exports = {
   production: {
     rules: [
       {
-        test: /\.s?css/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                importLoader: 2,
-                sourceMap: false,
-                minimize: true,
-                module: true,
-                localIndentName: '[name]__[local]_[hash:base64:5]'
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: [require('autoprefixer')({ browsers: 'ie >= 9' })]
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: { includePaths: [/scss/], sourceMap: false }
+        test: /\.(s?css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoader: 2,
+              sourceMap: false,
+              minimize: true,
             }
-          ]
-        })
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('autoprefixer')({ browsers: 'ie >= 9' })]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: { includePaths: [/scss/], sourceMap: false }
+          }
+        ]
       }
     ],
-    plugin: [
-        new ExtractTextPlugin({ filename: 'static/css/[name].[contenthash:8].css' })
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'static/css/[name].[contenthash:8].css',
+        chunkFilename: 'static/css/[id].[contenthash:8].css'
+      })
     ]
   }
 };
